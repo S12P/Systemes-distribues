cad1c8b7feba8251b09eafaf2b2a6ae31c8cf768
Stéphane <stephane.pouget@ens-lyon.fr>
2018-04-20 00:23:42 +0200

    pas mal

--- yop.erl
+++ yop.erl
@@ -78,20 +78,27 @@ ring_node(Father, ChildPid, Manager, Taille_ring, Root) ->
       Child ! {create, N-1},
       Manager ! {creation, Taille_ring, N},
       io:fwrite("ring ~p et ~p~n",[self(),Child]),
-      ring_node(self(), Child, Manager, Taille_ring,Root);
+      ring_node(self(), Child, Manager, Taille_ring, Root);
 
     {kill, PID} when self() == PID -> ChildPid ! {kill, self(), ChildPid}, ring_node(Father, ChildPid, Manager, Taille_ring, Root);
 
     {kill, PID, Child} when ChildPid == PID -> ChildPid = Child,
     ring_node(Father, Child, Manager, Taille_ring, Root); %pid de celui qu'on supprime et son fils
 
-    {add} -> Child = spawn(?MODULE, ring_node, [null, Manager, Taille_ring+1]),
+    {add} -> io:fwrite("add~p~n",[self()]),
+    Child = spawn(?MODULE, ring_node, [null, null, Manager, Taille_ring+1, null]),
+    io:fwrite("addChild ~p~n",[Child]),
     Child ! {add2, ChildPid},
-    ring_node(Father, Child, Manager, Taille_ring, Root),
-    ring_node(Father, ChildPid, Manager, Taille_ring, Root);
+    ring_node(self(), Child, Manager, Taille_ring+1, Root);
+    %ring_node(Father, ChildPid, Manager, Taille_ring+1, Root);
 
-    {add2, NewChild} -> ChildPid = NewChild,
-    ring_node(Father, ChildPid, Manager, Taille_ring, Root);
+    {add2, NewChild} -> io:fwrite("add2~p~p~p~n",[self(),NewChild,Father]),
+    io:fwrite("add22~p~p~p~n",[self(),NewChild,Father]),
+    NewChild ! {broadadd},
+    ring_node(self(), NewChild, Manager, Taille_ring, Root);
+
+% faire pour gerer taille ring 
+    %{broadadd} -> io:fwrite("Début broadcast~p~n",[ChildPid]), ChildPid ! {broadadd, 0, ChildPid}, ring_node(Father, ChildPid, Manager, Taille_ring+1,Root);
 
     {kill, PID, Child} -> ChildPid ! {kill, self(), ChildPid},
     ring_node(Father, ChildPid, Manager, Taille_ring, Root),
@@ -99,7 +106,7 @@ ring_node(Father, ChildPid, Manager, Taille_ring, Root) ->
 
     {broad} -> io:fwrite("Début broadcast~p~n",[ChildPid]), ChildPid ! {broad, 0, ChildPid}, ring_node(Father, ChildPid, Manager, Taille_ring,Root);
 
-    {broad, Nb, PID} -> if Taille_ring - Nb == 0 -> io:fwrite("Fin broadcast~p~n",[ChildPid]), ring_node(Father, ChildPid, Manager, Taille_ring,Root);
+    {broad, Nb, PID} -> if Taille_ring - Nb == 0 -> io:fwrite("Fin broadcast ~p~n",[ChildPid]), ChildPid ! {add}, ring_node(Father, ChildPid, Manager, Taille_ring,Root);
                         true -> ChildPid ! {broad, Nb + 1, ChildPid},
                         io:fwrite("Broadcast ~p ~p ~p~n",[ChildPid,Nb,Taille_ring]),
                         ring_node(Father, ChildPid, Manager, Taille_ring,Root) end
